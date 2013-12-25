de62164acec32626ce1356cbd921a8031a8e6057
Farooq Yousuf <farooqyousuf1@gmail.com>
2013-12-25 10:13:03 -0500

    First commit for FusionQA

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fusionqa::Application.config.secret_key_base = '637c017247b1ea59c5655010de35bdc1921af652471909c2035a375070f5f8ea8edc0f28c88e8ba77b264badf7fe3346468c828d2d45c569908e753b5014a904'
